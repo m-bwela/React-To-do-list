2586b4f9263d0e14f6429a6af68a33ad4df0fbb2
TyeMuzan <TyeMuzan@gmail.com>
2025-07-09 12:30:08 +0300

    modified components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,19 +1,14 @@
 import React, { useState } from "react";
 import ToDoItem from "./ToDoItem";
+import InputArea from "./InputArea";
+import Heading from "./Heading";
 
 function App() {
 
-const [inputText, setInputText] = useState("");
 const [listItems, setListItems] = useState([]);
 
-function handleChange(event) {
-  const newValue = event.target.value;
-  setInputText(newValue);
-}
-
-function handleClick() {
+function handleClick(inputText) {
   setListItems([...listItems, inputText]);
-  setInputText("");
 }
 
 function deleteItem(id) {
@@ -26,15 +21,10 @@ function deleteItem(id) {
 
   return (
     <div className="container">
-      <div className="heading">
-        <h1>To-Do List</h1>
-      </div>
-      <div className="form">
-        <input onChange={handleChange} type="text" value={inputText}/>
-        <button onClick={handleClick}>
-          <span>Add</span>
-        </button>
-      </div>
+      <Heading />
+      <InputArea 
+      clickAdd={handleClick}
+      />
       <div>
         <ul>
           {listItems.map((listItem, index) => {
